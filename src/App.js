b70b39c11101489098cbdb1cc2e3fba8a7f608ed
Luigi Brandolini <luigi.brandolini@gmail.com>
2019-09-24 13:33:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MyNavbar } from './components/shared/MyNavbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar></MyNavbar>
+    </div>
+  );
+}
+
+export default App;
